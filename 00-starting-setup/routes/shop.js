0d5ba0c52fff04df43580d4257a49b696c4b1e9c
austiniqer <63011075@kmitl.ac.th>
2021-06-16 15:13:45 +0700

    practise

---
+++ 00-starting-setup/routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const productController = require('../controllers/products')
+
+const router = express.Router();
+
+router.get('/',productController.getAllProducts );
+
+module.exports = router;
