7cb265c41a183177fb713a5dc769a24c30c9b95f
austiniqer <63011075@kmitl.ac.th>
2021-06-16 17:33:04 +0700

    Add more navigaion list

--- 00-starting-setup/routes/admin.js
+++ 00-starting-setup/routes/admin.js
@@ -2,15 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const productController = require('../controllers/products')
+const rootdir = require('../util/path');
+const adminController = require('../controllers/admin')
 const router = express.Router();
 
 
-// /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct);
+// /admin/add-product => get
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',productController.postAddProduct);
+router.post('/add-product',adminController.postAddProduct);
 
 exports.routes = router;
