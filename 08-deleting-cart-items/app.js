828b24693a3b8b07311134442b0d430c9bea43bb
austiniqer <63011075@kmitl.ac.th>
2021-06-18 21:58:34 +0700

    add user

--- 08-deleting-cart-items/app.js
+++ 08-deleting-cart-items/app.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const Product = require('./models/product')
+const User = require('./models/uesr')
 
 const sequelize = require("./util/database");
 const errorController = require("./controllers/error");
@@ -22,10 +24,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
 sequelize
   .sync()
   .then((result) => {
+    return User.findByPk(1)
     // console.log(result);
+  })
+  .then(user =>{
+    if(!user){
+      return User.create({name: 'austiniqer', email: 'dekkim6@gmail.com'})
+    }
+    return Promise.resolve(user);
+  })
+  .then(user =>{
+    // console.log(user)
     app.listen(3000);
   })
   .catch((err) => console.log(err));
