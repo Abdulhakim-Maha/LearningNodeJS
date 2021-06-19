18730baffb1c26c71b3e19de0a6b93c7c4c3e143
austiniqer <63011075@kmitl.ac.th>
2021-06-19 14:49:08 +0700

    working with mongoDB

---
+++ 13-resetting-the-cart-and-fetching/util/database.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (calback) => {
+  MongoClient.connect(
+    "mongodb+srv://austiniqer:austiniqer@cluster0.hqlbq.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db()
+      calback()
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if(_db){
+    return _db;
+  }else{
+    throw 'No database found!'
+  }
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
