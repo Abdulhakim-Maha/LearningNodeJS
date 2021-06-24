590aa45a9c18acf2376db768083f5c5a869a0434
austiniqer <63011075@kmitl.ac.th>
2021-06-24 17:39:29 +0700

    extra 2

--- mongodb/models/product.js
+++ mongodb/models/product.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb')
 const getDB = require("../util/database").getDB;
+
+const ObjectId = mongodb.ObjectID;
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -10,10 +13,37 @@ class Product {
 
   save() {
     const db = getDB();
-    return db.collection("products")
+    return db
+      .collection("products")
       .insertOne(this)
-      .then(result => {
-        console.log(result)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDB();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(prodId) {
+    const db = getDB();
+    return db
+      .collection("products")
+      .find({ _id: new ObjectId(prodId)})
+      .next()
+      .then(product => {
+        console.log(product)
+        return product;
       })
       .catch((err) => console.log(err));
   }
