e7f17a86a9e18d1331954977b590d253adf2f64c
austiniqer <63011075@kmitl.ac.th>
2021-06-25 16:40:57 +0700

    extra

--- mongoose/app.js
+++ mongoose/app.js
@@ -12,32 +12,44 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("5baa2528563f16379fc8a610")
+  User.findById("60d58882c7829a2b484f756b")
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
 });
 
-// app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://austiniqer:austiniqer@cluster0.hqlbq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://austiniqer:austiniqer@cluster0.hqlbq.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
-    console.log(result)
+    // console.log(result)
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "austiniqer",
+          email: "austin@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => console.log(err));
