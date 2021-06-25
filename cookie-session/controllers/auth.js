84bd4f510d84c15f839ce6f7589c7a094291b880
austiniqer <63011075@kmitl.ac.th>
2021-06-25 20:43:22 +0700

    session 1

--- cookie-session/controllers/auth.js
+++ cookie-session/controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res) => {
+  console.log(req.session.isLoggedIn)
   res.render("auth/login", {
     path: "/login",
     pageTitle: "login",
+    isLoggedIn: false,
   });
+};
+
+exports.postLogin = (req, res) => {
+  req.session.isLoggedIn = true
+  res.redirect("/");
 };
