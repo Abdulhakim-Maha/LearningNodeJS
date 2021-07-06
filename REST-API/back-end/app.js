a3adde798bbc96d6e2243f383feeb7466bd5828f
austiniqer <63011075@kmitl.ac.th>
2021-07-06 13:56:59 +0700

    connect to mongoose

--- REST-API/back-end/app.js
+++ REST-API/back-end/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
@@ -9,12 +10,23 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8000);
+mongoose
+  .connect(
+    "mongodb+srv://austiniqer:austiniqer@cluster0.hqlbq.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log("mongoose connected!");
+    app.listen(8000);
+  })
+  .catch((err) => console.log(err));
